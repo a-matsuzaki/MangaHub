85eaac58fc27ca84e251ea9f026dbdf63e5ebaa5
ATSUHIRO MATSUZAKI <55480334+matsuzaki0817@users.noreply.github.com>
2023-10-02 14:23:09 +0900

    SSLの設定

--- config/app.php
+++ config/app.php
@@ -3,186 +3,96 @@
 use Illuminate\Support\Facades\Facade;
 use Illuminate\Support\ServiceProvider;
 
+// Laravelの設定値を定義した配列を返すファイルです。
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Application Name
-    |--------------------------------------------------------------------------
-    |
-    | This value is the name of your application. This value is used when the
-    | framework needs to place the application's name in a notification or
-    | any other location as required by the application or its packages.
-    |
-    */
-
+    // -------------------------------------------------------------------------
+    // アプリケーションの名前
+    // -------------------------------------------------------------------------
+    // この値はアプリケーションの名前です。フレームワークが通知や他の場所でアプリケーションの名前を
+    // 表示する必要がある場合に使用されます。
     'name' => env('APP_NAME', 'Laravel'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Application Environment
-    |--------------------------------------------------------------------------
-    |
-    | This value determines the "environment" your application is currently
-    | running in. This may determine how you prefer to configure various
-    | services the application utilizes. Set this in your ".env" file.
-    |
-    */
-
+    // -------------------------------------------------------------------------
+    // アプリケーションの環境
+    // -------------------------------------------------------------------------
+    // この値はアプリケーションが現在実行されている"環境"を決定します。
     'env' => env('APP_ENV', 'production'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Application Debug Mode
-    |--------------------------------------------------------------------------
-    |
-    | When your application is in debug mode, detailed error messages with
-    | stack traces will be shown on every error that occurs within your
-    | application. If disabled, a simple generic error page is shown.
-    |
-    */
-
+    // -------------------------------------------------------------------------
+    // アプリケーションのデバッグモード
+    // -------------------------------------------------------------------------
+    // アプリケーションがデバッグモードにある場合、詳細なエラーメッセージが表示されます。
     'debug' => (bool) env('APP_DEBUG', false),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Application URL
-    |--------------------------------------------------------------------------
-    |
-    | This URL is used by the console to properly generate URLs when using
-    | the Artisan command line tool. You should set this to the root of
-    | your application so that it is used when running Artisan tasks.
-    |
-    */
-
+    // -------------------------------------------------------------------------
+    // アプリケーションのURL
+    // -------------------------------------------------------------------------
+    // このURLは、Artisanコマンドラインツールを使用してURLを正しく生成するためにコンソールに使用されます。
     'url' => env('APP_URL', 'http://localhost'),
 
+    // アセットのURL
     'asset_url' => env('ASSET_URL'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Application Timezone
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify the default timezone for your application, which
-    | will be used by the PHP date and date-time functions. We have gone
-    | ahead and set this to a sensible default for you out of the box.
-    |
-    */
-
+    // -------------------------------------------------------------------------
+    // アプリケーションのタイムゾーン
+    // -------------------------------------------------------------------------
+    // アプリケーションのデフォルトのタイムゾーンを指定できます。
     'timezone' => 'Asia/Tokyo',
 
-    /*
-    |--------------------------------------------------------------------------
-    | Application Locale Configuration
-    |--------------------------------------------------------------------------
-    |
-    | The application locale determines the default locale that will be used
-    | by the translation service provider. You are free to set this value
-    | to any of the locales which will be supported by the application.
-    |
-    */
-
+    // -------------------------------------------------------------------------
+    // アプリケーションのロケール設定
+    // -------------------------------------------------------------------------
+    // アプリケーションのデフォルトのロケールを指定します。
     'locale' => 'ja',
 
-    /*
-    |--------------------------------------------------------------------------
-    | Application Fallback Locale
-    |--------------------------------------------------------------------------
-    |
-    | The fallback locale determines the locale to use when the current one
-    | is not available. You may change the value to correspond to any of
-    | the language folders that are provided through your application.
-    |
-    */
-
+    // -------------------------------------------------------------------------
+    // アプリケーションのフォールバックロケール
+    // -------------------------------------------------------------------------
+    // 現在のロケールが利用できない場合に使用するロケールを指定します。
     'fallback_locale' => 'en',
 
-    /*
-    |--------------------------------------------------------------------------
-    | Faker Locale
-    |--------------------------------------------------------------------------
-    |
-    | This locale will be used by the Faker PHP library when generating fake
-    | data for your database seeds. For example, this will be used to get
-    | localized telephone numbers, street address information and more.
-    |
-    */
-
+    // -------------------------------------------------------------------------
+    // Fakerのロケール
+    // -------------------------------------------------------------------------
+    // データベースのシード用の偽のデータを生成する際に使用するロケールを指定します。
     'faker_locale' => 'ja_JP',
 
-    /*
-    |--------------------------------------------------------------------------
-    | Encryption Key
-    |--------------------------------------------------------------------------
-    |
-    | This key is used by the Illuminate encrypter service and should be set
-    | to a random, 32 character string, otherwise these encrypted strings
-    | will not be safe. Please do this before deploying an application!
-    |
-    */
-
+    // -------------------------------------------------------------------------
+    // 暗号化キー
+    // -------------------------------------------------------------------------
+    // このキーは暗号化サービスに使用されます。
     'key' => env('APP_KEY'),
 
     'cipher' => 'AES-256-CBC',
 
-    /*
-    |--------------------------------------------------------------------------
-    | Maintenance Mode Driver
-    |--------------------------------------------------------------------------
-    |
-    | These configuration options determine the driver used to determine and
-    | manage Laravel's "maintenance mode" status. The "cache" driver will
-    | allow maintenance mode to be controlled across multiple machines.
-    |
-    | Supported drivers: "file", "cache"
-    |
-    */
-
+    // -------------------------------------------------------------------------
+    // メンテナンスモードのドライバ
+    // -------------------------------------------------------------------------
+    // メンテナンスモードの状態を決定し、管理するためのドライバを指定します。
     'maintenance' => [
         'driver' => 'file',
         // 'store'  => 'redis',
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Autoloaded Service Providers
-    |--------------------------------------------------------------------------
-    |
-    | The service providers listed here will be automatically loaded on the
-    | request to your application. Feel free to add your own services to
-    | this array to grant expanded functionality to your applications.
-    |
-    */
-
+    // -------------------------------------------------------------------------
+    // 自動ロードされるサービスプロバイダ
+    // -------------------------------------------------------------------------
+    // ここにリストされているサービスプロバイダは、アプリケーションへのリクエスト時に自動的にロードされます。
     'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
     ])->toArray(),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Class Aliases
-    |--------------------------------------------------------------------------
-    |
-    | This array of class aliases will be registered when this application
-    | is started. However, feel free to register as many as you wish as
-    | the aliases are "lazy" loaded so they don't hinder performance.
-    |
-    */
+    // -------------------------------------------------------------------------
+    // クラスのエイリアス
+    // -------------------------------------------------------------------------
+    // この配列のクラスエイリアスはアプリケーションが開始されたときに登録されます。
+    'aliases' => Facade::defaultAliases()->merge([])->toArray(),
 
-    'aliases' => Facade::defaultAliases()->merge([
-        // 'Example' => App\Facades\Example::class,
-    ])->toArray(),
+    // SSLを強制するかどうか
+    'force_ssl' => env('FORCE_SSL', false)
 
 ];
