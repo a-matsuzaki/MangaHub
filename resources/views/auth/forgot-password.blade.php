be7d4c9993e466cc55830b11e8a41ca5c5754541
ATSUHIRO MATSUZAKI <55480334+matsuzaki0817@users.noreply.github.com>
2024-02-21 11:28:29 +0900

    TOPに戻るボタンの実装

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -16,7 +16,8 @@
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="flex items-center justify-between mt-4">
+            <a href="{{ url('/') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">TOPに戻る</a>
             <x-primary-button>
                 {{ __('Email Password Reset Link') }}
             </x-primary-button>
